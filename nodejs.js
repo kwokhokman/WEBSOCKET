dae447cc92b0fe31462ed6962a1a0f4343bb7d6e
hokman <435999329@qq.com>
2022-01-28 12:36:49 +0800

    1.3

--- nodejs.js
+++ nodejs.js
@@ -8,6 +8,7 @@ var server = ws.createServer(function (socket) {
         //向前端回复消息
         socket.sendText('服务器端收到客户端端发来的消息了！' + count++);
         console.log(1);
+        console.log(3);
     });
 
     socket.sendText('11111111111111111111111111111111！');
